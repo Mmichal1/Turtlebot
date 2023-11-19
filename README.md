7ecedc6f71045ec4c30a18c05513a7743807c50f
Michal Trela <mmichal.trela@gmail.com>
2023-11-19 18:10:14 +0100

    create the main package

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 ```bash
 ros2 launch nav2_bringup unique_multi_tb3_simulation_launch.py
 ```
+
+Even though that the model path is sourced on image build, gazebo doesn't acknowledge that and tries to download models form the internet which hangs and gazebo doesn't load properly. If it doesn't load, then you need to source it again:
+
+```bash
+GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:/home/ros/turtlebot3/turtlebot_mmrs/models
+```
---
+++ docker/turtlebot3/turtlebot_mmrs/setup.py
@@ -0,0 +1,35 @@
+import os
+from glob import glob
+from setuptools import setup
+
+package_name = 'turtlebot_mmrs'
+
+
+setup(
+    name=package_name,
+    version='0.1.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
+        (os.path.join('share', package_name, 'rviz'), glob('rviz/*.rviz')),
+        (os.path.join('share', package_name, 'maps'), glob('maps/*.pgm')),
+        (os.path.join('share', package_name, 'maps'), glob('maps/*.yaml')),
+        (os.path.join('share', package_name, 'params'), glob('params/*.yaml')),
+        (os.path.join('share', package_name, 'urdf'), glob('urdf/*.urdf')),
+        (os.path.join('share', package_name, 'worlds'), glob('worlds/*')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='ros',
+    maintainer_email='mmichal.trela@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
